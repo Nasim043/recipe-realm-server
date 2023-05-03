5ae019499bf2bea97d402e252ed96b21f67c669b
Saleh Nasim <nasimcse16@gmail.com>
2023-05-03 13:34:44 +0600

    chefs recipes list  from server

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = 3000;
 
-app.listen(port, ()=>{
+app.use(cors())
+const chefs = require('./data/Chef.json')
+const recipes = require('./data/recipes.json')
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs);
+})
+
+app.get('/chefs/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  let chefRecipes = recipes.filter(recipe => recipe.chef_id === id)
+  res.send(chefRecipes);
+})
+
+app.listen(port, () => {
   console.log(`Server run on port ${port}`);
 })
